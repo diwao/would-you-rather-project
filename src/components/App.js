22f80b96273eea7a9f730b04731ba3f76d398310
Daisuke Iwao <nakapom@gmail.com>
2021-02-14 18:24:36 +0900

    feat: Add getInitialData in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import { _getUsers, _getQuestions } from '../utils/_DATA';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard';
 import Question from './Question';
 import LeaderBoard from './LeaderBoard';
 import NewQuestion from './NewQuestion';
 class App extends Component {
+  async componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
+    const { authedUser } = this.props;
     return (
       <div>
-        <Dashboard />
-        <SignIn />
-        <Question />
-        <LeaderBoard />
-        <NewQuestion />
+        {authedUser !== null ? (
+          <SignIn />
+        ) : (
+          <div>
+            <Dashboard />
+            <Question />
+            <LeaderBoard />
+            <NewQuestion />
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
