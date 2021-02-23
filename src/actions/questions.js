87cd102e4f356f56c89e35811036e77006ed8d58
Daisuke Iwao <nakapom@gmail.com>
2021-02-24 00:41:12 +0900

    feat: Implement NewQuestion

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,4 @@
-import { saveQuestionAnswer } from '../utils/api';
+import { saveQuestion, saveQuestionAnswer } from '../utils/api';
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 export const ADD_QUESTION = 'ADD_QUESTION';
@@ -23,8 +23,28 @@ function answerQuestion({ authedUser, qid, answer }) {
 export function handleSaveQuestionAnswer({ qid, answer }) {
   return (dispatch, getState) => {
     const { authedUser } = getState();
-    saveQuestionAnswer({ authedUser, qid, answer }).then(() => {
+    return saveQuestionAnswer({ authedUser, qid, answer }).then(() => {
       dispatch(answerQuestion({ authedUser, qid, answer }));
+    });
+  };
+}
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+}
+
+export function handleSaveQuestion({ optionOneText, optionTwoText }) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author: authedUser,
+    }).then((question) => {
+      dispatch(addQuestion(question));
     });
   };
 }
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { handleSaveQuestion } from '../actions/questions';
 
 class NewQuestion extends Component {
+  state = {
+    optionOneText: '',
+    optionTwoText: '',
+  };
+
+  handleOptionOne = (e) => {
+    const optionOneText = e.target.value;
+    this.setState({
+      optionOneText,
+    });
+  };
+
+  handleOptionTwo = (e) => {
+    const optionTwoText = e.target.value;
+    this.setState({
+      optionTwoText,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { optionOneText, optionTwoText } = this.state;
+    const { dispatch } = this.props;
+    dispatch(handleSaveQuestion({ optionOneText, optionTwoText }));
+    this.setState(() => ({
+      optionOneText: '',
+      optionTwoText: '',
+    }));
+  };
+
   render() {
-    return <div>NewQuestion</div>;
+    const { optionOneText, optionTwoText } = this.state;
+    return (
+      <div>
+        <h2>Create New Quesiton</h2>
+        <p>Complete the question</p>
+        <form onSubmit={this.handleSubmit}>
+          <span>Would you rather than...</span>
+          <input
+            type="text"
+            placeholder="Enter option one here"
+            value={optionOneText}
+            onChange={this.handleOptionOne}
+          />
+          <span>Or</span>
+          <input
+            type="text"
+            placeholder="Enter option two here"
+            value={optionTwoText}
+            onChange={this.handleOptionTwo}
+          />
+          <button
+            type="submit"
+            disabled={optionOneText === '' || optionTwoText === ''}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
