253fa57ad960412eda09d9529a6c9124417b81c9
Daisuke Iwao <nakapom@gmail.com>
2021-02-28 16:42:01 +0900

    feat: Modify styles

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -24,22 +24,28 @@ class SignIn extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <h1>Would you rather</h1>
-        <p>Please Sign in</p>
+    const { selectedId } = this.state;
 
-        <h3>Sign in</h3>
-        <form onSubmit={this.handleSubmit}>
+    return (
+      <div className="text-center pt-10">
+        <h1 className="text-6xl">Would you rather</h1>
+        <p className="text-xl mt-10">Please Sign in</p>
+
+        <h3 className="text-2xl mt-5">Sign in</h3>
+        <form onSubmit={this.handleSubmit} className="mt-2">
           <select onChange={this.handleChange}>
-            <option value=""></option>
+            <option value="" disabled selected>
+              -
+            </option>
             {this.props.userIds.map((userId) => (
               <option value={userId} key={userId}>
                 {userId}
               </option>
             ))}
           </select>
-          <BaseButton type="submit">Sign in</BaseButton>
+          <BaseButton type="submit" disabled={selectedId === ''}>
+            Sign in
+          </BaseButton>
         </form>
       </div>
     );
