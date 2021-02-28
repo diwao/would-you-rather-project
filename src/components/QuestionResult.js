3fa166076a9f070c74ad83daa7027ba8b6773344
Daisuke Iwao <nakapom@gmail.com>
2021-03-01 00:03:01 +0900

    feat: Modify styles

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -12,28 +12,74 @@ export default function QuestionResult(props) {
   const totalVotes = optionOneVotes + optionTwoVotes;
 
   return (
-    <div>
-      <h3>{`Asked by ${author.name}`}</h3>
-      <div>
-        <img alt="name" src={author.avatarURL} />
-        <h4>Result</h4>
+    <article className="flex bg-gray-100 rounded-xl p-4 py-2 mt-4 items-center shadow border">
+      <div className="flex items-center">
+        <img
+          alt="name"
+          src={author.avatarURL}
+          className="w-32 h-32 rounded-full mx-auto"
+        />
+      </div>
+      <div className="flex-grow p-2 px-4">
+        <h3 className="font-semibold text-purple-600 text-lg">{`Asked by ${author.name}`}</h3>
+        <h4 className="font-semibold mt-2">Results:</h4>
         <ul>
-          <li>
-            <p>{`Would you rather ${question.optionOne.text}`}</p>
-            <span>{`${optionOneVotes}/${totalVotes} ${
-              (optionOneVotes / totalVotes) * 100
-            }%`}</span>
-            <p>{`vote: ${isSelected(question.optionOne, authedUser)}`}</p>
+          <li className="border border-purple-300 p-2">
+            <label htmlFor="resultOne" className="block">
+              {`Would you rather ${question.optionOne.text}`}
+              <spam
+                className={`bg-purple-500 text-white px-1 ml-1 ${
+                  isSelected(question.optionOne, authedUser)
+                    ? 'inline-block'
+                    : 'hide'
+                }`}
+              >
+                Your Vote
+              </spam>
+            </label>
+            <div className="mt-1">
+              <progress
+                id="resultOne"
+                max="100"
+                value={Math.round((optionOneVotes / totalVotes) * 100)}
+              >{`${Math.round(
+                (optionOneVotes / totalVotes) * 100
+              )}%`}</progress>
+              <span>{`${Math.round(
+                (optionOneVotes / totalVotes) * 100
+              )}%`}</span>
+            </div>
+            <span className="block">{`${optionOneVotes} out of ${totalVotes} votes`}</span>
           </li>
-          <li>
-            <p>{`Would you rather ${question.optionTwo.text}`}</p>
-            <span>{`${optionTwoVotes}/${totalVotes} ${
-              (optionTwoVotes / totalVotes) * 100
-            }%`}</span>
-            <p>{`votes: ${isSelected(question.optionTwo, authedUser)}`}</p>
+          <li className="border border-purple-300 p-2 mt-2">
+            <label htmlFor="resultTwo" className="block">
+              {`Would you rather ${question.optionTwo.text}`}
+              <spam
+                className={`bg-purple-500 text-white px-1 ml-1 ${
+                  isSelected(question.optionTwo, authedUser)
+                    ? 'inline-block'
+                    : 'hide'
+                }`}
+              >
+                Your Vote
+              </spam>
+            </label>
+            <div className="mt-1">
+              <progress
+                id="resultTwo"
+                max="100"
+                value={Math.round((optionTwoVotes / totalVotes) * 100)}
+              >{`${Math.round(
+                (optionTwoVotes / totalVotes) * 100
+              )}%`}</progress>
+              <span>{`${Math.round(
+                (optionTwoVotes / totalVotes) * 100
+              )}%`}</span>
+            </div>
+            <span className="block">{`${optionOneVotes} out of ${totalVotes} votes`}</span>
           </li>
         </ul>
       </div>
-    </div>
+    </article>
   );
 }
