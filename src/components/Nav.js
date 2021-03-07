6c35261ef0d1c957af27f677ab98756a88bbe54a
Daisuke Iwao <nakapom@gmail.com>
2021-03-07 16:24:03 +0900

    feat: Style Navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,20 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <NavLink to="/" exact activeClassName="active">
+    <nav className="flex items-center justify-between flex-wrap bg-purple-500 p-6">
+      <ul className="w-full block flex-grow">
+        <li className="inline-block mt-0 text-purple-200 hover:text-white mr-4">
+          <NavLink to="/" exact activeClassName="italic font-bold">
             Home
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/add" activeClassName="active">
+        <li className="inline-block mt-0 text-purple-200 hover:text-white mr-4">
+          <NavLink to="/add" activeClassName="italic font-bold">
             New Question
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/leaderboard" activeClassName="active">
+        <li className="inline-block mt-0 text-purple-200 hover:text-white mr-4">
+          <NavLink to="/leaderboard" activeClassName="italic font-bold">
             Leader Board
           </NavLink>
         </li>
